bdc83d3222e8d2f70cc91d1bab22206fcb6099f3
Bulat <safin.b111@gmail.com>
2019-11-22 16:05:20 +0300

    'add' command added

--- rss_it_jump_bot.py
+++ rss_it_jump_bot.py
@@ -1,10 +1,12 @@
+#region Imports
 from telegram.ext import Updater, CommandHandler
 from bs4 import BeautifulSoup
 import feedparser
 import urllib.request
 import telegram
+#endregion
 
-
+#region Util functions
 def get_user_data(data_path):
     with open(data_path, 'rt') as data_file:
         data_list = data_file.read().split('\n')
@@ -23,7 +25,6 @@ def set_user_data(data_path, user_data):
     with open(data_path, 'wt') as data_file:
         data_file.write('\n'.join(data_list))
 
-
 def get_rss_feed1(website_url):
     page = urllib.request.urlopen(website_url)
     soup = BeautifulSoup(page)
@@ -32,24 +33,30 @@ def get_rss_feed1(website_url):
     if link is None:
         return 'no link'
     return link['href']
+#endregion
 
-
-telegram_token = '1025022667:AAGy4d57cRfbZAOXsNM5W2rvRPYKegyttgM'
-updater = Updater(telegram_token, use_context=True)
-dispather = updater.dispatcher
-
-
+#region Start
 def start(update, context):
+    user_data = get_user_data('user_db.txt')
+    tg_user = update.message.from_user
+
+    if not tg_user.id in user_data.keys():
+        user_data[tg_user.id] = [tg_user.first_name, tg_user.is_bot, tg_user.username, tg_user.language_code, []]
+
+    set_user_data('user_db.txt', user_data)
+
     context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я — бот-новостной агрегатор, разработанный на смене IT Jump Pro\
 \nНапиши мне /help и я скажу тебе как мной пользоваться")
+#endregion
 
-
+#region Help
 def help(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text='Напиши мне /read {ссылка на новостной ресурс} и я \
 покажу тебе последние статьи этого сайта.\
 \nНапример: /read https://dtf.ru')
+#endregion
 
-
+#region Read
 def read(update, context):
     args = update.message.text.split()
     del args[0]
@@ -89,12 +96,48 @@ def read(update, context):
 published at {article_published_at}
 ----------------------------
 {article_link}'''
-        print(msg)
         context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
+#endregion
 
+#region Add
+def add(update, context):
+    args = update.message.text.split()
+    arg_url = args[1]
+    del args
+    user_data = get_user_data('user_db.txt')
+    tg_user = update.message.from_user
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f'Ищу RSS ленту на {arg_url}...')
+
+    feed_url = get_rss_feed1(arg_url)
+    if feed_url == 'no link':
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'Извините, я не нашёл RSS ленту на этом'
+                                                                        f' сайте. Попробуйте ввести другую ссылку')
+        return
+
+    if feed_url.startswith('/'):
+        feed_url = arg_url + feed_url
+
+    print(user_data, tg_user.id, feed_url)
+    if(not feed_url in user_data[str(tg_user.id)][-1]):
+        user_data[str(tg_user.id)][-1].append(feed_url)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'Теперь вы подписаны на {feed_url}')
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Этот сайт уже есть среди ваших подписок')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f'Список ваших подписок: '
+                                                                    f'{user_data[str(tg_user.id)][-1]}')
+    set_user_data('user_db.txt', user_data)
+#endregion
+
+#region Setup
+telegram_token = '1025022667:AAGy4d57cRfbZAOXsNM5W2rvRPYKegyttgM'
+updater = Updater(telegram_token, use_context=True)
+dispather = updater.dispatcher
 
 dispather.add_handler(CommandHandler("start", start))
 dispather.add_handler(CommandHandler("help", help))
 dispather.add_handler(CommandHandler("read", read))
+dispather.add_handler(CommandHandler("add", add))
 
 updater.start_polling()
+#endregion
