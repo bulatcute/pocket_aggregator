6f8212aee7ab941d1ea5da9b562911231004c329
Kam Lotfull <kamlotfull@gmail.com>
2019-11-23 12:26:00 +0300

    Fixes `databases.py`

--- databases.py
+++ databases.py
@@ -1,30 +1,40 @@
+from __future__ import absolute_import, print_function
+
 from pony.orm import *
 
-db = Database()
+db = Database("sqlite", "bot.sqlite", create_db=True)
+
+
 class User(db.Entity):
     user_id = Required(int)
     sites = Set('Feed')
+
 
 class Feed(db.Entity):
     url = Required(str)
     users = Set(User)
 
-db.bind(provider='sqlite', filename='datebase.sqlite', create_db=True)
 
 @db_session
 def add_feed_user(feed, user):
-    Feed.users.append(feed)
+    feed.users.add(user)
+
 
 @db_session
 def add_user(id):
-    User(user_id=id)
+    return User(user_id=id)
+
 
 @db_session
 def add_feed(url):
-    Feed(url=url)
+    return Feed(url=url)
 
-u1 = add_user(32424542)
-f1 = add_feed('https://dtf.ru')
 
-add_feed_user(f1, u1)[:]
-print(select(p for p in User))
+db.generate_mapping(create_tables=True)
+
+with db_session:
+    u1 = add_user(32424542)
+    f1 = add_feed('https://dtf.ru')
+
+    add_feed_user(f1, u1)
+    print(select(p for p in User)[:])
